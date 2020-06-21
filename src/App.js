08f98929d70f4b9c11cde45df6c37f766bb8392f
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-06-21 14:53:58 +0100

    Add menu icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as MenuIcon } from './Icons/menu.svg';
 import './styles.css';
 
 export default function App() {
@@ -6,6 +7,16 @@ export default function App() {
     <div className="App">
       <h1>Hello Light switch</h1>
       <h2>Start editing to see some magic happen!</h2>
+      Icons made by{' '}
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        Freepik
+      </a>{' '}
+      from{' '}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        {' '}
+        www.flaticon.com
+      </a>
+      <MenuIcon />
     </div>
   );
 }
