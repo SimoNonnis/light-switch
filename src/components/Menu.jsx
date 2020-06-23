51726389bd7a5b96638131fa4c4cd8764e93a09b
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-06-23 17:43:12 +0100

    Style menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { ReactComponent as CancelIcon } from '../Icons/cancel.svg';
 
+const menuItems = [
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
+];
+
 const Menu = ({ setIsOpen }) => (
   <div className="menu">
-    <CancelIcon onClick={() => setIsOpen(false)} className="icon" />
+    <CancelIcon onClick={() => setIsOpen(false)} className="icon icon--close" />
+    <ul className="menuList">
+      {menuItems.map(item => (
+        <li key={item} className="menuListItem">
+          {item}
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
