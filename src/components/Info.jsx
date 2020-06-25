d0f998459ec1d0df8f8a4ceaa10fce625b76aca2
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-06-25 17:39:59 +0100

    Add details

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,6 +7,18 @@ import { ReactComponent as InfoIcon } from '../Icons/info.svg';
 const Info = () => (
   <div className="content">
     <div className="content">
+      <ul>
+        <li className="infoItem">Заказчик: Покупатель Покупателевич</li>
+        <li className="infoItem">Email: amserik@yandex.ru</li>
+        <li className="infoItem">
+          Адрес: 1155487, г Москва, ул Садовая Б., д 49
+        </li>
+        <li className="infoItem">Телефон: +7 906 266-55-63</li>
+        <li className="infoItem">Исполнитель: Группа компаний</li>
+        <li className="infoItem">Менеджер: Шроков Евгений</li>
+        <li className="infoItem">Телефон: +7(499)123-35-01</li>
+        <li className="infoItem">Монтаж: Да</li>
+      </ul>
       <Credits />
     </div>
 
